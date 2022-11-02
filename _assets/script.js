39f2fc337e52244d46f2349c70ad0836868a3795
DaBlincx <blincxyt@gmail.com>
2022-11-02 21:01:12 +0100

    Added mobile nav-menu support

--- _assets/script.js
+++ _assets/script.js
@@ -7,3 +7,12 @@ function playRR() {
     var rr = new Audio('/_assets/audio/rr.mp3');
     rr.play();
 }
+
+function revColMenu() {
+    var x = document.getElementById("navbar-menu");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
