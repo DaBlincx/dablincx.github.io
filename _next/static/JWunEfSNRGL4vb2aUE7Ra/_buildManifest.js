1164d54411eaa5b0d334b80651868213442d1198
DaBlincx <blincxyt@gmail.com>
2023-07-25 22:46:11 +0200

    Updates

--- _next/static/JWunEfSNRGL4vb2aUE7Ra/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-393277eed7c6f3ea.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
