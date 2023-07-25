6d595f22d48311465a351e1fb58eb0ba2048bb57
DaBlincx <blincxyt@gmail.com>
2023-07-26 00:03:04 +0200

    yes i did copy the default 404 page

--- pages/404.js
+++ pages/404.js
@@ -3,21 +3,10 @@ import Link from 'next/link';
 export default function error404() {
     return (
         <div className="error-404-container">
-            <div style={{
-                fontSize: '24px',
-                fontWeight: '500',
-                color: '#FFFFFF',
-                borderRight: '1px solid rgba(255,255,255,.3)',
-                paddingRight: '23px',
-                marginRight: '20px',
-                lineHeight: '48px',
-            }}>
+            <div className='error-404-number'>
                 404
             </div>
-            <div style={{
-                fontSize: '14px',
-                fontWeight: '400',
-            }}>
+            <div className='error-404-text'>
                 This page could not be found.<br/>
                 Go back to <Link href="/" className='error-404-link'>Home</Link>
             </div>
