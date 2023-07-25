d7922528d645f79b493e3037c32602fa31713ad9
DaBlincx <blincxyt@gmail.com>
2023-07-25 23:15:03 +0200

    image test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'media.discordapp.net',
+                port: '',
+                pathname: '/attachments/**/**/**',
+            }
+        ],
+    }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
 export default function Home() {
@@ -7,20 +8,30 @@ export default function Home() {
                 background: 'linear-gradient(180deg, #000000 0%, #1B1B1B 100%)',
                 height: '100vh',
                 width: '100vw',
-                position: 'relative',
             }
         }>
-            <h1 style={{
-                color: '#FFFFFF',
-                fontFamily: 'Inter',
-                top: '50%',
+            <div style={{
                 position: 'absolute',
+                top: '50%',
                 left: '50%',
                 transform: 'translate(-50%, -50%)',
-                fontSize: '4rem',
+                textAlign: 'center',
+                width: '100%',
             }}>
-                hello cool people, me is editing this website (also i gotta test smth)
-            </h1>
+                <h1 style={{
+                    color: '#FFFFFF',
+                    fontFamily: 'Inter',
+                    fontSize: '4rem',
+                }}>
+                    hello cool people, me is editing this website (also i gotta test smth)
+                </h1>
+                <Image
+                    src="https://media.discordapp.net/attachments/1124714078162653244/1131305790079254598/bombcat.gif"
+                    alt="bombcat"
+                    width="500"
+                    height="600"
+                />
+            </div>
         </div>
   )
 }
