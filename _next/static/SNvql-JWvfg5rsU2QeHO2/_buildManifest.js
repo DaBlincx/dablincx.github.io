e4eecc8998ae0ca0079ab726b20a43f08c3afec9
DaBlincx <blincxyt@gmail.com>
2023-07-28 17:48:48 +0200

    Updates

---
+++ _next/static/SNvql-JWvfg5rsU2QeHO2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-46c070e333689db5.js"],"/404":["static/chunks/pages/404-d918bb34996db5a6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
