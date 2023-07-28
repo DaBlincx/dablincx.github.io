cef210d633f4052b9ef3456cd8dd7e33725f477f
DaBlincx <blincxyt@gmail.com>
2023-07-28 17:41:18 +0200

    hrgl

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
             <body style={{
                 margin: 0,
                 padding: 0,
+                background: 'linear-gradient(180deg, #000000 0%, #1B1B1B 100%)',
                 }}>
                 <Main />
                 <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -3,37 +3,29 @@ import { Inter } from 'next/font/google'
 
 export default function Home() {
     return (
-        <div style={
-            {
-                background: 'linear-gradient(180deg, #000000 0%, #1B1B1B 100%)',
-                height: '100vh',
-                width: '100vw',
-            }
-        }>
-            <div style={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                transform: 'translate(-50%, -50%)',
-                textAlign: 'center',
-                width: '100vw',
-                height: '100vh',
+        <div style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            textAlign: 'center',
+            width: '100vw',
+            height: '100vh',
+        }}>
+            <h1 style={{
+                color: '#FFFFFF',
+                fontFamily: 'Inter',
+                fontSize: '4rem',
             }}>
-                <h1 style={{
-                    color: '#FFFFFF',
-                    fontFamily: 'Inter',
-                    fontSize: '4rem',
-                }}>
-                    hello cool people, me is editing this website (also i gotta test smth)<br/>
-                    :3
-                </h1>
-                <Image
-                    src="https://media.discordapp.net/attachments/1124714078162653244/1131305790079254598/bombcat.gif"
-                    alt="bombcat"
-                    width="500"
-                    height="600"
-                />
-            </div>
+                hello cool people, me is editing this website (also i gotta test smth)<br/>
+                :3
+            </h1>
+            <Image
+                src="https://media.discordapp.net/attachments/1124714078162653244/1131305790079254598/bombcat.gif"
+                alt="bombcat"
+                width="500"
+                height="600"
+            />
         </div>
     )
 }
