d19c023d9058c50acd63d04357d18968cbe4a63e
DaBlincx <blincxyt@gmail.com>
2023-08-02 21:35:10 +0200

    moved css into css file

--- pages/_navbar.js
+++ pages/_navbar.js
@@ -15,37 +15,14 @@ function NavBar({ bgColor }) {
         navbar.classList.toggle('navbar-open');
     }
 
-    if (bgColor == null) {
-        bgColor = 'transparent';
-    }
+    if (bgColor == null) { bgColor = 'transparent'; }
     return (
-        <div style={{
-            fontSize: '1.3rem',
-        }}>
+        <div style={{ fontSize: '1.3rem' }}>
             <div id="navbar-toggle" className="" onClick={toggleNavbar}>
                 Menu
             </div>
-            <div id="navbar" className="" style={{
-                position: 'fixed',
-                top: '0',
-                right: '-350px',
-                height: '100vh',
-                width: '350px',
-                background: '#000000',
-                display: 'flex',
-                justifyContent: 'right',
-                alignItems: 'right',
-                padding: '1rem 1.5rem',
-                paddingRight: '1.5rem',
-                transition: '0.5s',
-                zIndex: '2',
-                fontSize: '1.3rem',
-                //what the fuck is this god damn
-            }}>
-                <div style={{
-                    color: '#FFFFFF',
-                    width: '100%'
-                }}>
+            <div id="navbar" className="">
+                <div style={{ width: '100%' }}>
                     <div className="navbar-menu" onClick={toggleNavbar}>
                         <div>Menu</div>
                         <div className="navbar-menu-arrow"></div>
